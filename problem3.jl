0097c87bba4ba3b6bfa25c84ce051a845a0afd35
Samuel <swiqvist@gmail.com>
2019-04-17 18:22:43 +0200

    done with problem a) and b), still have to do c)

--- problem3.jl
+++ problem3.jl
@@ -12,7 +12,7 @@ PyPlot.figure()
 h = PyPlot.plt[:hist](waiting,10,density= true)
 
 
-# function to compute the kde 
+# function to compute the kde
 function kde(data::Vector, grid::Vector, kernel::Function, bandwidth::Real)
 
     n = length(data)
@@ -21,7 +21,7 @@ function kde(data::Vector, grid::Vector, kernel::Function, bandwidth::Real)
 
     for i = 1:nbr_grid_points
         for j = 1:n
-            kde_est[i] = kde_est[i] + kernel((gird[i]-data[j])/bandwidth)
+            kde_est[i] = kde_est[i] + kernel((grid[i]-data[j])/bandwidth)
         end
     end
 
@@ -43,6 +43,8 @@ PyPlot.figure()
 h = PyPlot.plt[:hist](waiting,20, density= true)
 PyPlot.plot(kde_grid, kde_est)
 
+# task a) Uniform kernel with bandwidth = 3 work ok, we also tried using the Gaussian kernel,
+# but the unifrom kernal seemed to work better
 
 # rejection sampling algortihm
 function rejectionsampling(p::Function, g::Function, sample::Function, M::Real, N::Int)
@@ -54,10 +56,10 @@ function rejectionsampling(p::Function, g::Function, sample::Function, M::Real, 
 
         generate_proposal = true
 
-        while true
+        while generate_proposal
             proposal = sample()
             r = p(proposal)/(M*g(proposal))
-            if rand() < r
+            if rand() <= r
                 samples[i] = proposal
                 generate_proposal = false
             end
@@ -74,10 +76,17 @@ end
 p(x) = kde(waiting, [x], k_uniform, bandwidth)[1]
 g(x) = 1
 sample() = 40 + (110-40)*rand()
-M = 0.005
+M = 0.05
 
-samples = rejectionsampling(p, g, sample, M, 10)
+samples = @time rejectionsampling(p, g, sample, M, 10000)
+
+# task b) We use unifrom proposal dist on [40,110], we get 10000 samples in 0.05 sec. Hard to
+# find better prop dist since the kde is bimodal, could use a mixture of Gaussians but that
+# is too complicated for this simple problem
 
 PyPlot.figure()
 h = PyPlot.plt[:hist](samples, 100, density= true)
 PyPlot.plot(kde_grid, kde_est)
+
+
+# task c) bootstrap confidence band of what? 
